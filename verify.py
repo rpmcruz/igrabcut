4401e492d1cf6efab4c0442401b05b7e5262abe9
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2021-03-20 13:15:22 +0000

    Added key to advance. Crop to bounding box to make it easier to verify images.

--- verify.py
+++ verify.py
@@ -6,12 +6,24 @@ parser.add_argument('dirname')
 args = parser.parse_args()
 
 import matplotlib.pyplot as plt
+import numpy as np
 import cv2
 from skimage import color, io
 import math
 import os
 
 seg_filenames = [os.path.join(args.dirname, f) for f in sorted(os.listdir(args.dirname)) if f.endswith('-seg.png')]
+
+def key_pressed(ev):
+    if ev.key in ('enter', 'escape'):
+        plt.close()
+
+def mask_bbox(mask, border=10):
+    a = np.where(mask)
+    return (np.maximum(np.min(a[0])-border, 0),
+        np.minimum(np.max(a[0])+border, mask.shape[0]),
+        np.maximum(np.min(a[1])-border, 0),
+        np.minimum(np.max(a[1])+border, mask.shape[1]))
 
 size = 2
 end = math.ceil(len(seg_filenames)/size)*size
@@ -23,6 +35,9 @@ for i in range(0, end, size):
         img = io.imread(img_filename, True)
 
         img = color.gray2rgb(img)
+        bbox = mask_bbox(mask)
+        img = img[bbox[0]:bbox[1], bbox[2]:bbox[3]]
+        mask = mask[bbox[0]:bbox[1], bbox[2]:bbox[3]]
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
 
@@ -33,4 +48,5 @@ for i in range(0, end, size):
         plt.title(os.path.basename(img_filename))
     plt.suptitle(f'{i+1}-{i+size} / {end}')
     plt.get_current_fig_manager().window.showMaximized()
+    plt.gcf().canvas.mpl_connect('key_press_event', key_pressed)
     plt.show()
